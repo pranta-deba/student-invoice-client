278a2065297c602e8bf7abb67a21dd6bfe4493c1
Pranto Deb <rajprit0m@outlook.com>
2025-07-26 16:00:18 +0600

    [Profile Dropdown]

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
-import { UserCircle } from "lucide-react";
 import useAuth from "../../hooks/useAuth";
 import ProfileDropdown from "../ui/profile-dropdown";
 
