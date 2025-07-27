1fabd1bb9005811984abad2599f70004b9ea3c26
Pranto Deb <rajprit0m@outlook.com>
2025-07-27 14:40:50 +0600

    [Tailwind Init]

---
+++ src/components/layouts/root.tsx
@@ -0,0 +1,14 @@
+import Footer from "../shared/footer";
+import Navbar from "../shared/navbar";
+
+const Root = () => {
+  return (
+    <>
+      <Navbar />
+      <main>main</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Root;
